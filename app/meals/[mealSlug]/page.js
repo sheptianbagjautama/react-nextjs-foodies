f957093eff6dee26f2bf467b5366e7b0fee3f498
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-19 15:02:46 +0700

    Throwing not found errors for individual meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  //jika meal tidak ada arahkan ke halaman not found
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
 
