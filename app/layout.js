215807c0e4dd2de993d6b476f2381bfe7e4499f1
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-20 15:39:21 +0700

    Adding static metadata

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "NextLevel Food",
-  description: "Delicious meals, shared by a food-loving community.",
+  description: "Delici ous meals, shared by a food-loving community.",
 };
 
 export default function RootLayout({ children }) {
